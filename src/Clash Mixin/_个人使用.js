a968eb9638641dab5fc89978b7e31a0762da0cb6
Jason Liang <lainbo@foxmail.com>
2023-06-28 19:13:40 +0800

    style: 🎨 lint

--- src/Clash Mixin/_个人使用.js
+++ src/Clash Mixin/_个人使用.js
@@ -77,7 +77,11 @@ function main(content) {
   // 额外的DNS设置
   const extraDNS = {
     'default-nameserver': ['223.5.5.5', '223.6.6.6', '119.29.29.29'],
-    'nameserver': ['https://dns.alidns.com/dns-query', 'https://doh.pub/dns-query', 'https://doh.opendns.com/dns-query'],
+    'nameserver': [
+      'https://dns.alidns.com/dns-query',
+      'https://doh.pub/dns-query',
+      'https://doh.opendns.com/dns-query',
+    ],
     'fallback': ['1.1.1.1', '208.67.222.222'],
   }
 
