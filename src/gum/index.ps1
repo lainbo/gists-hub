0e9fe6583caa6f65227b9a0ca15ac812261a70e9
Jason Liang <lainbo@foxmail.com>
2023-07-09 01:17:30 +0800

    feat: ✨ 添加跳过不合规文件夹判断

--- src/gum/index.ps1
+++ src/gum/index.ps1
@@ -1,10 +1,18 @@
 $folderPath = $PSScriptRoot
 $directories = Get-ChildItem -Path $folderPath -Directory
+
 foreach ($directory in $directories) {
-  Set-Location -Path $directory.FullName
-  Write-Output "文件夹 $($directory.FullName) 开始执行"
-  gum use github
-  Set-Location -Path $folderPath
+  $gitDirectory = Join-Path $directory.FullName ".git"
+  
+  if(Test-Path -Path $gitDirectory){
+    Set-Location -Path $directory.FullName
+    Write-Output "文件夹 $($directory.FullName) 开始执行"
+    gum use github
+    Set-Location -Path $folderPath
+  }
+  else{
+    Write-Output "文件夹 $($directory.FullName) 跳过执行"
+  }
 }
 
 Write-Output "执行完毕"
