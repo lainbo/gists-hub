ff86fb9b653f650ee794c92c5f02ed51774c0a92
Jason Liang <lainbo@foxmail.com>
2023-09-25 10:11:39 +0800

    perf: ⚡ 优化节点信息的正则

--- src/uTools/一步到位/ip-api查询.js
+++ src/uTools/一步到位/ip-api查询.js
@@ -1,8 +1,19 @@
-const url = 'https://ip-api.com/'
 const inputStr = ENTER.payload.trim()
+const ipApiUrl = 'https://ip-api.com'
+const fraudUrl = 'https://scamalytics.com'
+const ipInfoUrl = 'https://ipinfo.io'
+const ipapiIsUrl = 'https://ipapi.is'
+const ipapiIsApiUrl = 'https://api.ipapi.is'
+
 const reg = /^((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])(?::(?:[0-9]|[1-9][0-9]{1,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5]))?$/
 if (reg.test(inputStr)) {
-  utools.shellOpenExternal(`${url}#${inputStr}`)
+  utools.shellOpenExternal(`${ipApiUrl}/#${inputStr}`)
+  utools.shellOpenExternal(`${fraudUrl}/ip/${inputStr}`)
+  utools.shellOpenExternal(`${ipInfoUrl}/${inputStr}`)
+  utools.shellOpenExternal(`${ipapiIsApiUrl}/?q=${inputStr}`)
 } else {
-  utools.shellOpenExternal(url)
+  utools.shellOpenExternal(ipApiUrl)
+  utools.shellOpenExternal(fraudUrl)
+  utools.shellOpenExternal(ipInfoUrl)
+  utools.shellOpenExternal(ipapiIsUrl)
 }
