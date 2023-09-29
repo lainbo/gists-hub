c75a753313ac2e6b337caaf9169e1f7a4bf089f9
Jason Liang <lainbo@foxmail.com>
2023-09-29 14:49:48 +0800

    refactor: ♻️ 抽离下载配置函数

--- src/QuantumultX/RemoteConfig/script/downloadQx.js
+++ src/QuantumultX/RemoteConfig/script/downloadQx.js
@@ -1,50 +1,35 @@
-// 用于将 Gist 上的配置文件下载到本地，会覆盖本地的 QxTemplate.conf 模板文件
-import { promises as fs } from 'node:fs'
 import path from 'node:path'
 import process from 'node:process'
 import url from 'node:url'
-import { Octokit } from '@octokit/core'
 import dotenv from 'dotenv'
 import { config } from './_config.js'
+import { downloadConfig } from '#src/_utils/downloadFromGist.js'
+
+dotenv.config()
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
 
-dotenv.config()
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN
-const GIST_ID = process.env.QX_GIST_ID
-const FILE_NAME = config.githubGistFileName
-
-const octokit = new Octokit({
-  auth: GITHUB_TOKEN,
-})
-
+// 下载后的内容处理函数
 function replaceContent(content) {
-  // 将gist上配置文件中的订阅替换成标识符，防止失误上传到仓库
   const serverRemoteReg = /\[server_remote\]([\s\S]*?)\[filter_remote\]/
   const serverFlagStr = '[server_remote]\n# {$server_remote}\n\n[filter_remote]'
-
-  // doh行替换为注释，防止失误上传到仓库
   const dohReg = /doh-server=([^\n]+)/
+
   content = content.replace(serverRemoteReg, serverFlagStr).replace(dohReg, '# doh-server=')
   return content
 }
 
-async function downloadQx() {
-  try {
-    const response = await octokit.request(`GET /gists/${GIST_ID}`, {
-      gist_id: GIST_ID,
-    })
+async function main() {
+  const outputPath = path.join(__dirname, '../QxTemplate.conf')
 
-    if (response.data.files && response.data.files[FILE_NAME]) {
-      const content = replaceContent(response.data.files[FILE_NAME].content)
-      const outputPath = path.join(__dirname, '../QxTemplate.conf')
-      await fs.writeFile(outputPath, content, 'utf-8')
-      console.log('下载成功,已更新模板,文件在', outputPath)
-    }
-  }
-  catch (error) {
-    console.error(`发生错误: ${error}`)
-  }
+  await downloadConfig({
+    githubToken: process.env.GITHUB_TOKEN,
+    gistId: process.env.QX_GIST_ID,
+    fileName: config.githubGistFileName,
+    outputPath: outputPath,
+    contentProcessor: replaceContent,
+    appName: 'QuantumultX'
+  })
 }
 
-downloadQx()
+main()
---
+++ src/_utils/downloadFromGist.js
@@ -0,0 +1,37 @@
+import { Octokit } from '@octokit/core'
+import fs from 'node:fs/promises'
+
+// 下载并处理文件内容的纯工具函数
+export async function downloadConfig({
+  githubToken,
+  gistId,
+  fileName,
+  outputPath,
+  contentProcessor,
+  appName
+}) {
+  const octokit = new Octokit({
+    auth: githubToken,
+  })
+
+  try {
+    const response = await octokit.request(`GET /gists/${gistId}`, {
+      gist_id: gistId,
+    })
+
+    if (response.data.files && response.data.files[fileName]) {
+      let content = response.data.files[fileName].content
+
+      // 调用传入的内容处理函数
+      if (typeof contentProcessor === 'function') {
+        content = contentProcessor(content)
+      }
+
+      await fs.writeFile(outputPath, content, 'utf-8')
+      console.log(`下载${appName}配置成功,已更新模板,文件在 ${outputPath}`)
+    }
+  }
+  catch (error) {
+    console.error(`发生错误: ${error}`)
+  }
+}
