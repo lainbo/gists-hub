56df2e9a70200c35327bd86b373c9b397a29f587
Jason Liang <lainbo@foxmail.com>
2023-11-15 23:21:03 +0800

    feat: ✨ 添加github workflow

---
+++ process-files.js
@@ -0,0 +1,26 @@
+const fs = require('node:fs')
+const path = require('node:path')
+
+const files = [
+  'gists-hub/src/QuantumultX/List/BanAD.list',
+  'gists-hub/src/QuantumultX/List/BanEasyList.list',
+  'gists-hub/src/QuantumultX/List/BanEasyListChina.list',
+]
+
+files.forEach(file => {
+  const filePath = path.join(__dirname, file)
+  const data = fs.readFileSync(filePath, 'utf8')
+  const lines = data.split('\n')
+
+  const processedLines = lines.map(line => {
+    if (line.startsWith('DOMAIN')) {
+      return `${line}, Proxy`
+    }
+    else if (line.startsWith('IP-CIDR') && line.endsWith(',no-resolve')) {
+      return line.replace(',no-resolve', ',Proxy,no-resolve')
+    }
+    return line
+  })
+
+  fs.writeFileSync(filePath, processedLines.join('\n'))
+})
